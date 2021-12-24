139fda1fd87a2b8fd738ff597a5259583dd6544d
Jingcheng Wu <wu_jingcheng@qq.com>
2021-12-24 12:18:54 -0500

    finalized stoptrain feature

--- back-end/utils/train.py
+++ back-end/utils/train.py
@@ -134,3 +134,7 @@ class TrainThread(threading.Thread):
     def run(self):
         call_back, epochs, auto_save = self.args
         self.trainer.start_train(call_back, epochs, auto_save)
+    
+    def stop(self):
+        print("Setting trainer stop flag...")
+        self.trainer.stop = True
