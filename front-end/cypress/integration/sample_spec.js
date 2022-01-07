db3d518711d2cfe2a7ab81c88311b44e74b8d06a
Desmond <l840376507@outlook.com>
2022-01-07 20:17:22 +0800

    Update sample_spec.js

--- front-end/cypress/integration/sample_spec.js
+++ front-end/cypress/integration/sample_spec.js
@@ -18,3 +18,21 @@ describe('My First Visiting Test', () => {
     cy.url().should('include', '/commands/actions')
   })
 })
+
+describe('Hooks', () => {
+  before(() => {
+    // runs once before all tests in the block
+  })
+
+  beforeEach(() => {
+    // runs before each test in the block
+  })
+
+  afterEach(() => {
+    // runs after each test in the block
+  })
+
+  after(() => {
+    // runs once after all tests in the block
+  })
+})
