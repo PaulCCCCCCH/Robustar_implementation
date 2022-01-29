25baf4f5185e7d8f0ecae5d8110fd9f482f64286
lxn <scyxl12@nottingham.edu.cn>
2022-01-29 18:32:50 +0800

    config for favicon

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  transpileDependencies: ['vuetify'],
+    transpileDependencies: ['vuetify'],
 };
+
+module.exports = {
+    pluginOptions: {
+        // ...
+        pwa: {
+            iconPaths: {
+                favicon32: './favicon.ico',
+                favicon16: './favicon.ico',
+                appleTouchIcon: './favicon.ico',
+                maskIcon: './favicon.ico',
+                msTileImage: './favicon.ico'
+            }
+        },
+    }
+}
