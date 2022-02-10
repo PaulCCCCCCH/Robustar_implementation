0beb88a83dd4e54ceaa72c45e7d679c94d08cb50
Leon-Leyang <scylh6@nottingham.edu.cn>
2022-02-10 11:54:14 +0800

    Refactor the server control part with docker-py

--- launcher/launcher.py
+++ launcher/launcher.py
@@ -1,5 +1,7 @@
 import json
 import subprocess
+import time
+
 import docker
 
 from PySide2.QtUiTools import QUiLoader
@@ -130,45 +132,106 @@ class Launcher(QWidget):
             # Restart the container
             if self.container.status == 'exited':
                 self.container.restart()
+                print('The server is now restarted')
+                self.ui.messageBrowser.append('Robustar is available at http://localhost:' + self.configs['portNumber'])
+                self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
+                QApplication.processEvents()
             # If the container is running
             elif self.container.status == 'running':
                 print('The server has already been running')
+                self.ui.messageBrowser.append('The server has already been running')
+                self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
+                QApplication.processEvents()
             # If the container is in other status
             else:
                 print('Unexpected status')
+                self.ui.messageBrowser.append('The server encountered an unexpected status')
+                self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
+                QApplication.processEvents()
+                time.sleep(5)
                 exit(1)
         # If the container with the input name has not been created yet
         # Create a new container and run it
         except docker.errors.NotFound:
-            self.container = self.client.containers.run(image,
-                                                        detach=True,
-                                                        name=self.configs['containerName'],
-                                                        ports={
-                                                            '80/tcp': (
-                                                                '127.0.0.1', int(self.configs['portNumber'])),
-                                                            '8000/tcp': ('127.0.0.1', 6848),
-                                                            '6006/tcp': ('127.0.0.1', 6006),
-                                                        },
-                                                        mounts=[
-                                                            docker.types.Mount(target='/Robustar2/dataset/train',
-                                                                               source=self.configs['trainPath'],
-                                                                               type='bind'),
-                                                            docker.types.Mount(target='/Robustar2/dataset/test',
-                                                                               source=self.configs['testPath'],
-                                                                               type='bind'),
-                                                            docker.types.Mount(target='/Robustar2/influence_images',
-                                                                               source=self.configs['influencePath'],
-                                                                               type='bind'),
-                                                            docker.types.Mount(
-                                                                target='/Robustar2/checkpoint_images ',
-                                                                source=self.configs['checkPointPath'],
-                                                                type='bind'),
-                                                        ],
-                                                        volumes=[
-                                                            self.configs['configFile'] + ':/Robustar2/configs.json']
-                                                        )
+            # If the version uses cuda
+            # Set the device_requests parm
+            if 'cuda' in image:
+                self.container = self.client.containers.run(
+                    image,
+                    detach=True,
+                    name=self.configs['containerName'],
+                    ports={
+                        '80/tcp': (
+                            '127.0.0.1', int(self.configs['portNumber'])),
+                        '8000/tcp': ('127.0.0.1', 6848),
+                        '6006/tcp': ('127.0.0.1', 6006),
+                    },
+                    mounts=[
+                        docker.types.Mount(target='/Robustar2/dataset/train',
+                                           source=self.configs['trainPath'],
+                                           type='bind'),
+                        docker.types.Mount(target='/Robustar2/dataset/test',
+                                           source=self.configs['testPath'],
+                                           type='bind'),
+                        docker.types.Mount(target='/Robustar2/influence_images',
+                                           source=self.configs['influencePath'],
+                                           type='bind'),
+                        docker.types.Mount(
+                            target='/Robustar2/checkpoint_images ',
+                            source=self.configs['checkPointPath'],
+                            type='bind'),
+                    ],
+                    volumes=[
+                        self.configs['configFile'] + ':/Robustar2/configs.json'],
+                    device_requests=[
+                        docker.types.DeviceRequest(count=-1, capabilities=[['gpu']])
+                    ]
+                )
+                print('The server is now started')
+                self.ui.messageBrowser.append('Robustar is available at http://localhost:' + self.configs['portNumber'])
+                self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
+                QApplication.processEvents()
+            # If the version only uses cpu
+            else:
+                self.container = self.client.containers.run(
+                    image,
+                    detach=True,
+                    name=self.configs['containerName'],
+                    ports={
+                        '80/tcp': (
+                            '127.0.0.1', int(self.configs['portNumber'])),
+                        '8000/tcp': ('127.0.0.1', 6848),
+                        '6006/tcp': ('127.0.0.1', 6006),
+                    },
+                    mounts=[
+                        docker.types.Mount(target='/Robustar2/dataset/train',
+                                           source=self.configs['trainPath'],
+                                           type='bind'),
+                        docker.types.Mount(target='/Robustar2/dataset/test',
+                                           source=self.configs['testPath'],
+                                           type='bind'),
+                        docker.types.Mount(target='/Robustar2/influence_images',
+                                           source=self.configs['influencePath'],
+                                           type='bind'),
+                        docker.types.Mount(
+                            target='/Robustar2/checkpoint_images ',
+                            source=self.configs['checkPointPath'],
+                            type='bind'),
+                    ],
+                    volumes=[
+                        self.configs['configFile'] + ':/Robustar2/configs.json']
+                )
+                print('The server is now started')
+                self.ui.messageBrowser.append('Robustar is available at http://localhost:' + self.configs['portNumber'])
+                self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
+                QApplication.processEvents()
         except docker.errors.APIError:
             print('The server returns an error')
+            self.ui.messageBrowser.append('The server encountered an error')
+            self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
+            QApplication.processEvents()
+            time.sleep(5)
+            exit(1)
 
 
 
@@ -177,85 +240,101 @@ class Launcher(QWidget):
         try:
             self.container = self.client.containers.get(self.configs['containerName'])
             self.container.stop()
+            # If the container is running
+            # Stop the container
+            if self.container.status == 'exited':
+                print('The server has already been stopped')
+                self.ui.messageBrowser.append('The server has already been stopped')
+                self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
+                QApplication.processEvents()
+            # If the container is running
+            elif self.container.status == 'running':
+                self.container.stop()
+                print('The server is now stopped')
+                self.ui.messageBrowser.append('The server is now stopped')
+                self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
+                QApplication.processEvents()
+            # If the container is in other status
+            else:
+                print('Unexpected status')
+                self.ui.messageBrowser.append('The server encountered an unexpected status')
+                self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
+                QApplication.processEvents()
+                time.sleep(5)
+                exit(1)
         except docker.errors.NotFound:
             print('The server has not been created yet')
+            self.ui.messageBrowser.append('The server has not been created yet')
+            self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
+            QApplication.processEvents()
         except docker.errors.APIError:
             print('The server returns an error')
-
-
-
-
-
+            self.ui.messageBrowser.append('The server encountered an error')
+            self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
+            QApplication.processEvents()
+            time.sleep(5)
+            exit(1)
 
 
 
     # Concatenate the command to be executed
-    def getRunCommand(self):
-        runCommand = 'docker run --name ' + self.configs['containerName'] + ' -d ' +\
-                       '-p 127.0.0.1:' + self.configs['portNumber'] + ':80 ' +\
-                       '-p 127.0.0.1:6848:8000 ' +\
-                       '-p 127.0.0.1:6006:6006 ' +\
-                       '--mount type=bind,source=' + self.configs['trainPath'] + ',target=/Robustar2/dataset/train ' +\
-                       '--mount type=bind,source=' + self.configs['testPath'] + ',target=/Robustar2/dataset/test ' +\
-                       '--mount type=bind,source=' + self.configs['influencePath'] + ',target=/Robustar2/influence_images ' +\
-                       '--mount type=bind,source=' + self.configs['checkPointPath'] + ',target=/Robustar2/checkpoint_images ' +\
-                       '-v ' + self.configs['configFile'] + ':/Robustar2/configs.json ' +\
-                       'paulcccccch/robustar:' + self.configs['imageVersion']
-
-        return runCommand
-
-
-
-
-
-
-
-
-
+    # def getRunCommand(self):
+    #     runCommand = 'docker run --name ' + self.configs['containerName'] + ' -d ' +\
+    #                    '-p 127.0.0.1:' + self.configs['portNumber'] + ':80 ' +\
+    #                    '-p 127.0.0.1:6848:8000 ' +\
+    #                    '-p 127.0.0.1:6006:6006 ' +\
+    #                    '--mount type=bind,source=' + self.configs['trainPath'] + ',target=/Robustar2/dataset/train ' +\
+    #                    '--mount type=bind,source=' + self.configs['testPath'] + ',target=/Robustar2/dataset/test ' +\
+    #                    '--mount type=bind,source=' + self.configs['influencePath'] + ',target=/Robustar2/influence_images ' +\
+    #                    '--mount type=bind,source=' + self.configs['checkPointPath'] + ',target=/Robustar2/checkpoint_images ' +\
+    #                    '-v ' + self.configs['configFile'] + ':/Robustar2/configs.json ' +\
+    #                    'paulcccccch/robustar:' + self.configs['imageVersion']
+    #
+    #     return runCommand
 
     # Run a shell command and update UI according to it
-    def runShellCommand(self, command):
-        process = subprocess.Popen([r'C:/Program Files/Git/bin/bash.exe', "-c", command],
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE,
-                                   universal_newlines=True)
-
-        while True:
-            output = process.stdout.readline()
-            error = process.stderr.readline()
-            if (len(output) != 0):
-                print(output.strip())
-                self.ui.messageBrowser.append(output.strip())
-                self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
-                QApplication.processEvents()
-            if (len(error) != 0):
-                print(error.strip())
-                self.ui.messageBrowser.append(error.strip())
-                self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
-                QApplication.processEvents()
-
-            # Check if the process has finished
-            return_code = process.poll()
-            if return_code is not None:
-                # print('RETURN CODE', return_code)
-                # self.ui.messageBrowser.append('RETURN CODE ' + str(return_code))
-                # self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
-                # QApplication.processEvents()
-
-                # Process has finished, read rest of the output
-                for output in process.stdout.readlines():
-                    if (len(output) != 0):
-                        print(output.strip())
-                        self.ui.messageBrowser.append(output.strip())
-                        self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
-                        QApplication.processEvents()
-                break
-
-        self.ui.messageBrowser.append('\n')
-        self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
-        QApplication.processEvents()
-
-        return return_code
+    # def runShellCommand(self, command):
+    #     process = subprocess.Popen([r'C:/Program Files/Git/bin/bash.exe', "-c", command],
+    #                                stdout=subprocess.PIPE,
+    #                                stderr=subprocess.PIPE,
+    #                                universal_newlines=True)
+    #
+    #     while True:
+    #         output = process.stdout.readline()
+    #         error = process.stderr.readline()
+    #         if (len(output) != 0):
+    #             print(output.strip())
+    #             self.ui.messageBrowser.append(output.strip())
+    #             self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
+    #             QApplication.processEvents()
+    #         if (len(error) != 0):
+    #             print(error.strip())
+    #             self.ui.messageBrowser.append(error.strip())
+    #             self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
+    #             QApplication.processEvents()
+    #
+    #         # Check if the process has finished
+    #         return_code = process.poll()
+    #         if return_code is not None:
+    #             # print('RETURN CODE', return_code)
+    #             # self.ui.messageBrowser.append('RETURN CODE ' + str(return_code))
+    #             # self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
+    #             # QApplication.processEvents()
+    #
+    #             # Process has finished, read rest of the output
+    #             for output in process.stdout.readlines():
+    #                 if (len(output) != 0):
+    #                     print(output.strip())
+    #                     self.ui.messageBrowser.append(output.strip())
+    #                     self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
+    #                     QApplication.processEvents()
+    #             break
+    #
+    #     self.ui.messageBrowser.append('\n')
+    #     self.ui.messageBrowser.moveCursor(self.ui.messageBrowser.textCursor().End)
+    #     QApplication.processEvents()
+    #
+    #     return return_code
 
 
 app = QApplication([])
