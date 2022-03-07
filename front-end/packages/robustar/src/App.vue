7b64843d8e166dd550014db3c127f29a1280c0f2
Jingcheng Wu <jingchew@andrew.cmu.edu>
2022-03-06 19:55:07 -0500

    refactoring done

--- front-end/packages/robustar/src/App.vue
+++ front-end/packages/robustar/src/App.vue
@@ -1,60 +1,10 @@
 <template>
   <v-app>
-    <v-app-bar app color="white">
-      <!-- Robustar Logo -->
-      <a href="index">
-        <img src="./assets/images/brand/logo.png" style="width: 130px" alt="logo" />
-      </a>
-
-      <v-spacer></v-spacer>
-
-      <!-- Full screen button -->
-      <v-btn icon color="primary" @click="toggleTaskspanel">
-        <v-icon>mdi-format-list-bulleted-type</v-icon>
-      </v-btn>
-      <v-btn icon color="primary" @click="toggleFullscreen">
-        <v-icon v-if="!isFullscreen">mdi-fullscreen</v-icon>
-        <v-icon v-else>mdi-fullscreen-exit</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <Header @toggleTaskspanel="toggleTaskspanel"></Header>
     <SideBar></SideBar>
 
     <v-main class="page-content">
-      <v-row v-if="!isTaskspanelHidden" align="center" justify="center">
-        <v-col cols="12" lg="6"></v-col>
-        <v-col cols="12" lg="6">
-
-        <v-card
-          style="position:fixed; width: 40%; z-index:10; padding-top:3rem; padding-bottom: 3rem; align: center;"
-          elevation="4"
-        >
-          <v-row align="center" justify="center">
-            <v-col cols="12" lg="12" align="center" justify="center" v-if="digest.length==0">
-              <p style="color: gray;">No task is running now.</p>
-            </v-col>
-          </v-row>
-          <v-row v-for="(item, index) in digest" align="center" justify="center">
-              <v-col cols="12" lg="1" align="center" justify="center">
-                <v-btn color="red" icon @click="stopTask(item[3])"><v-icon>mdi-minus-box</v-icon></v-btn>
-              </v-col>
-              <v-col cols="12" lg="2" align="center" justify="center">{{item[0]}}</v-col>
-              <v-col cols="12" lg="5" align="center" justify="center">
-                <v-progress-linear
-                  color="primary"
-                  height="20"
-                  v-bind:value="item[1]*100"
-                  striped
-                >{{Math.round(item[1]*10000)/100}}%</v-progress-linear>
-              </v-col>
-              <v-col cols="12" lg="4" align="center" justify="center">{{item[2]}}</v-col>
-          </v-row>
-        
-        </v-card>
-        
-        </v-col>
-      </v-row>
-
+      <TaskPanel v-if="!isTaskspanelHidden"></TaskPanel>
       <Notification></Notification>
       <router-view />
     </v-main>
@@ -66,7 +16,7 @@
 import Header from '@/components/common/Header';
 import SideBar from '@/components/common/SideBar';
 import Notification from '@/components/common/Notification';
-import { APIStopTask } from '@/apis/task';
+import TaskPanel from '@/components/common/TaskPanel';
 
 export default {
   name: 'App',
@@ -74,57 +24,31 @@ export default {
     Header,
     SideBar,
     Notification,
-  },
-  data() {
-    return {
-      isFullscreen: false,
-      isTaskspanelHidden: true,
-      training: 6,
-      nonTraining: 6,
-      progress: 50,
-      // socket: io('wss://localhost:8000/'),
-      digest: [],
-    };
-  },
-  sockets: {
-    connect(){
-      console.log('connect');
-    },
-    afterConnect(data){
-      console.log(data);
-    },
-
-    // get digest from backend
-    digest(data){
-      this.digest = data.digest;
-    },
+    TaskPanel,
   },
   methods: {
-    toggleFullscreen() {
-      if (!document.fullscreenElement) {
-        document.documentElement.requestFullscreen();
-      } else {
-        if (document.exitFullscreen) {
-          document.exitFullscreen();
-        }
-      }
-      this.isFullscreen = !this.isFullscreen;
-    },
     toggleTaskspanel() {
       this.isTaskspanelHidden = !this.isTaskspanelHidden;
     },
-    stopTaskSuccess(res){
-        console.log(res);
-        alert("Successfully stop task");
-    },
-    stopTaskFailed(res){
-        console.log(res);
-        alert("Failed to stop task");
-    },
-    stopTask(tid){
-        APIStopTask(tid, this.stopTaskSuccess, this.stopTaskFailed);
-    }
   },
+  data() {
+    return {
+      isTaskspanelHidden: true,
+    };
+  },
+  // sockets: {
+  //   connect(){
+  //     console.log('connect');
+  //   },
+  //   afterConnect(data){
+  //     console.log(data);
+  //   },
+
+  //   // get digest from backend
+  //   digest(data){
+  //     this.digest = data.digest;
+  //   },
+  // },
 };
 </script>
 
