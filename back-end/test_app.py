6194cd4d6d1211ac42b2d57bf1670dff02a0450c
Yuhao Zhang <yhao.zhang98@gmail.com>
2022-03-28 00:06:26 +0100

    FIX: change corresponding to current project structure

--- back-end/test_app.py
+++ back-end/test_app.py
@@ -1,18 +1,25 @@
-from objects.RServer import RServer
-from objects.RDataManager import RDataManager
-import pytest
 import json
+import os.path as osp
 
-from server import create_server
+import pytest
 
-create_server()
+from objects.RServer import RServer
+
+baseDir = osp.join('/', 'Robustar2').replace('\\', '/')
+datasetDir = osp.join(baseDir, 'dataset').replace('\\', '/')
+ckptDir = osp.join(baseDir, 'checkpoints').replace('\\', '/')
+with open(osp.join(baseDir, 'configs.json')) as jsonfile:
+    configs = json.load(jsonfile)
+
+RServer.createServer(configs=configs, baseDir=baseDir, datasetDir=datasetDir, ckptDir=ckptDir)
 
 def test_valid_app_and_server():
     server = RServer.getServer()
     assert server
     assert server.getFlaskApp()
-    assert server.dataManager 
-    assert RServer.getModelWrapper()
+    # assert server.dataManager
+    # assert RServer.getModelWrapper()
+
 
 @pytest.fixture()
 def client():
@@ -21,12 +28,11 @@ def client():
     with app.test_client() as client:
         yield client
 
+
 class TestPredict:
 
     def test_predict(self, client):
         rv = client.get("/backend_test/train/1")
         res = json.loads(rv.data)
         pred_arr = res['data']
-        assert len(pred_arr[0])==len(pred_arr[1])
-    
-
+        assert len(pred_arr[0]) == len(pred_arr[1])
