4f706ade4c34bc963d1a354b7596220f3a29566a
Desmond <scysl4@nottingham.edu.cn>
2022-06-11 21:05:46 +0800

    feat: add image (start/end index) selector in ImageList

--- front-end/packages/robustar/src/views/ImageList.vue
+++ front-end/packages/robustar/src/views/ImageList.vue
@@ -1,98 +1,145 @@
 <template>
   <div class="d-flex justify-center align-center" style="height: 100%">
     <div class="d-flex flex-column flex-grow-1 align-center py-8">
-      <!-- sticky header: settings -->
-      <div class="d-flex flex-column align-center rounded px-8 elevation-2 sticky-header">
-        <div
-          v-if="$route.params.split === 'validation' || $route.params.split === 'test'"
-          style="width: 200px"
+      <div class="sticky-header d-flex mb-4">
+        <!-- image list settings -->
+        <v-sheet
+          class="d-flex flex-column align-center justify-center rounded px-8 mr-4 elevation-2"
+          color="white"
         >
-          <v-select
-            v-model="split"
-            :items="classification"
-            @change="resetImageList"
-            data-test="image-list-select-classification"
-          ></v-select>
-        </div>
-
-        <!-- Page navigator -->
-        <div class="d-flex justify-center align-center my-4">
-          <!-- Previous page button -->
-          <v-btn
-            :disabled="currentPage <= 0 || !hasImages"
-            depressed
-            color="primary"
-            @click="currentPage--"
-            data-test="image-list-btn-prev-page"
+          <div
+            v-if="$route.params.split === 'validation' || $route.params.split === 'test'"
+            style="width: 200px"
           >
-            PREV PAGE
-          </v-btn>
-
-          <!-- Refresh page button & page number -->
-          <div class="d-flex align-center mx-8">
-            <v-btn class="mr-4" depressed color="primary" @click="gotoPage"> GOTO PAGE </v-btn>
-            <v-text-field
-              data-test="image-list-input-page-number"
-              v-model="inputPage"
-              label="Page Number"
-              type="number"
-            ></v-text-field>
+            <v-select
+              v-model="split"
+              :items="classification"
+              @change="resetImageList"
+              data-test="image-list-select-classification"
+            ></v-select>
           </div>
 
-          <!-- Next page button -->
-          <v-btn
-            data-test="image-list-btn-next-page"
-            :disabled="currentPage >= maxPage || !hasImages"
-            depressed
-            color="primary"
-            @click="currentPage++"
-          >
-            NEXT PAGE
-          </v-btn>
-
-          <v-divider vertical class="mx-8"></v-divider>
-
-          <!-- Class filter -->
-          <div class="d-flex align-center">
+          <!-- Page navigator -->
+          <div class="d-flex justify-center align-center my-4">
+            <!-- Previous page button -->
             <v-btn
-              :disabled="selectedClass === 0"
-              class="mr-4"
+              :disabled="currentPage <= 0 || !hasImages"
               depressed
               color="primary"
-              @click="gotoClass"
+              @click="currentPage--"
+              data-test="image-list-btn-prev-page"
             >
-              GOTO CLASS
+              PREV PAGE
             </v-btn>
+
+            <!-- Refresh page button & page number -->
+            <div class="d-flex align-center mx-8">
+              <v-btn class="mr-4" depressed color="primary" @click="gotoPage"> GOTO PAGE </v-btn>
+              <v-text-field
+                data-test="image-list-input-page-number"
+                v-model="inputPage"
+                label="Page Number"
+                type="number"
+              ></v-text-field>
+            </div>
+
+            <!-- Next page button -->
+            <v-btn
+              data-test="image-list-btn-next-page"
+              :disabled="currentPage >= maxPage || !hasImages"
+              depressed
+              color="primary"
+              @click="currentPage++"
+            >
+              NEXT PAGE
+            </v-btn>
+
+            <v-divider vertical class="mx-8"></v-divider>
+
+            <!-- Class filter -->
+            <div class="d-flex align-center">
+              <v-btn
+                :disabled="selectedClass === 0"
+                class="mr-4"
+                depressed
+                color="primary"
+                @click="gotoClass"
+              >
+                GOTO CLASS
+              </v-btn>
+              <v-select
+                :items="classNames"
+                v-model="selectedClass"
+                label="Class Name"
+                data-test="image-list-select-class-name"
+              >
+              </v-select>
+            </div>
+          </div>
+
+          <!-- row & col settings -->
+          <div class="d-flex">
             <v-select
-              :items="classNames"
-              v-model="selectedClass"
-              label="Class Name"
-              data-test="image-list-select-class-name"
+              :items="imagePerPageOptions"
+              v-model="imagePerPage"
+              label="Number of image per page"
+              class="mr-8"
+              outlined
+              @change="resetImageList"
+            >
+            </v-select>
+            <v-select
+              :items="Object.keys(imageSizeMap)"
+              v-model="imageSize"
+              label="Image size"
+              outlined
+              @change="setImageSize"
             >
             </v-select>
           </div>
-        </div>
+        </v-sheet>
 
-        <!-- row & col settings -->
-        <div class="d-flex">
-          <v-select
-            :items="imagePerPageOptions"
-            v-model="imagePerPage"
-            label="Number of image per page"
-            class="mr-8"
-            outlined
-            @change="resetImageList"
-          >
-          </v-select>
-          <v-select
-            :items="Object.keys(imageSizeMap)"
-            v-model="imageSize"
-            label="Image size"
-            outlined
-            @change="setImageSize"
-          >
-          </v-select>
-        </div>
+        <!-- influence calculation & auto annotate -->
+        <v-sheet class="d-flex align-center justify-center rounded px-8 elevation-2" color="white">
+          <div v-if="showExtraSettings" class="mr-4">
+            <p>Click image to select its index</p>
+            <v-radio-group v-model="imageIdxSelection" mandatory row>
+              <v-radio :label="`Start Index : ${absoluteStartIdx}`" value="start"></v-radio>
+              <v-radio :label="`End Index : ${absoluteEndIdx}`" value="end"></v-radio>
+            </v-radio-group>
+            <v-btn
+              depressed
+              outlined
+              color="primary"
+              class="mr-4"
+              @click="
+                $router.push({
+                  name: 'InfluencePad',
+                  params: { startIdx: absoluteStartIdx, endIdx: absoluteEndIdx },
+                })
+              "
+            >
+              <v-icon class="mr-2">mdi-vector-link</v-icon> Influence
+            </v-btn>
+            <v-btn
+              depressed
+              outlined
+              color="primary"
+              @click="
+                $router.push({
+                  name: 'AutoAnnotatePad',
+                  params: { startIdx: absoluteStartIdx, endIdx: absoluteEndIdx },
+                })
+              "
+            >
+              <v-icon class="mr-2">mdi-auto-fix</v-icon> Auto Annotate
+            </v-btn>
+          </div>
+          <v-btn icon color="grey" large @click="showExtraSettings = !showExtraSettings">
+            <v-icon v-if="!showExtraSettings">mdi-chevron-double-right</v-icon>
+            <v-icon v-else>mdi-chevron-double-left</v-icon>
+          </v-btn>
+        </v-sheet>
       </div>
       <v-divider class="mb-8 mt-4" style="width: 85%"></v-divider>
 
@@ -107,57 +154,63 @@
           :cols="imageSizeMap[imageSize]"
           data-test="image-list-div-all-imgs"
         >
-          <!-- class="d-flex child-flex" -->
-          <div class="d-flex align-right">
-            <v-btn
-              v-if="$route.params.split === 'annotated'"
-              color="secondary"
-              class="mr-n1 mb-n1 mx-auto"
-              icon
-              small
-              @click="deleteAnnotatedImage(idx, url)"
+          <v-hover :disabled="showExtraSettings" v-slot="{ hover }">
+            <v-badge
+              :value="showExtraSettings && (idx === imageStartIdx || idx === imageEndIdx)"
+              bordered
+              color="warning"
+              icon="mdi-check"
+              overlap
             >
-              <v-icon color="red">mdi-close-box</v-icon>
-            </v-btn>
-          </div>
-          <v-hover v-slot="{ hover }">
-            <v-img
-              :src="url"
-              alt="invalid image URL"
-              aspect-ratio="1"
-              :data-test="`image-list-img-${idx}`"
-            >
-              <template v-slot:placeholder>
-                <v-row class="fill-height ma-0" align="center" justify="center">
-                  <v-progress-circular indeterminate color="primary lighten-3">
-                  </v-progress-circular>
-                </v-row>
-              </template>
-
-              <v-expand-transition>
-                <div
-                  v-if="hover"
-                  class="d-flex flex-column transition-fast-in-fast-out primary v-card--reveal"
-                  style="height: 100%"
-                >
-                  <v-btn
-                    class="mb-4"
-                    outlined
-                    color="white"
-                    width="80%"
-                    @click="gotoImage(url, 'EditImage')"
-                    :data-test="`image-list-btn-edit-image-${idx}`"
+              <v-img
+                :src="url"
+                alt="invalid image URL"
+                aspect-ratio="1"
+                :data-test="`image-list-img-${idx}`"
+                @click="selectImage(idx)"
+              >
+                <template v-slot:placeholder>
+                  <v-row class="fill-height ma-0" align="center" justify="center">
+                    <v-progress-circular indeterminate color="primary lighten-3">
+                    </v-progress-circular>
+                  </v-row>
+                </template>
+                <v-expand-transition>
+                  <div
+                    v-if="hover"
+                    class="d-flex flex-column transition-fast-in-fast-out primary v-card--reveal"
+                    style="height: 100%"
                   >
-                    <v-icon>mdi-pencil</v-icon>
-                    <span v-if="imageSize !== 'extra small'" class="ml-2">ANNOTATE</span>
-                  </v-btn>
-                  <v-btn outlined color="white" width="80%" @click="setCurrentImage(url)">
-                    <v-icon>mdi-cogs</v-icon>
-                    <span v-if="imageSize !== 'extra small'" class="ml-2">PREDICT</span>
-                  </v-btn>
-                </div>
-              </v-expand-transition>
-            </v-img>
+                    <v-btn
+                      class="mb-4"
+                      outlined
+                      color="white"
+                      width="80%"
+                      @click="gotoImage(url, 'EditImage')"
+                      :data-test="`image-list-btn-edit-image-${idx}`"
+                    >
+                      <v-icon>mdi-pencil</v-icon>
+                      <span v-if="imageSize !== 'extra small'" class="ml-2">ANNOTATE</span>
+                    </v-btn>
+                    <v-btn outlined color="white" width="80%" @click="setCurrentImage(url)">
+                      <v-icon>mdi-cogs</v-icon>
+                      <span v-if="imageSize !== 'extra small'" class="ml-2">PREDICT</span>
+                    </v-btn>
+                    <v-btn
+                      v-if="$route.params.split === 'annotated'"
+                      class="mt-4"
+                      outlined
+                      color="white"
+                      width="80%"
+                      @click="deleteAnnotatedImage(idx, url)"
+                    >
+                      <v-icon>mdi-delete</v-icon>
+                      <span v-if="imageSize !== 'extra small'" class="ml-2">DELETE</span>
+                    </v-btn>
+                  </div>
+                </v-expand-transition>
+              </v-img>
+            </v-badge>
           </v-hover>
         </v-col>
       </v-row>
@@ -189,6 +242,10 @@ export default {
   },
   data() {
     return {
+      showExtraSettings: false,
+      imageIdxSelection: 'start',
+      imageStartIdx: 0,
+      imageEndIdx: 22,
       currentPage: 0,
       inputPage: 0,
       maxPage: 0,
@@ -244,6 +301,12 @@ export default {
       const imagePerRow = 12 / this.imageSizeMap[this.imageSize];
       // images per page is a multiple of imagePerRow
       return new Array(5).fill().map((option, index) => imagePerRow * (index + 1));
+    },
+    absoluteStartIdx() {
+      return this.currentPage * this.imagePerPage + this.imageStartIdx;
+    },
+    absoluteEndIdx() {
+      return this.currentPage * this.imagePerPage + this.imageEndIdx;
     },
   },
   methods: {
@@ -357,6 +420,15 @@ export default {
         }
       );
     },
+    selectImage(idx) {
+      if (this.showExtraSettings) {
+        if (this.imageIdxSelection === 'start') {
+          this.imageStartIdx = idx;
+        } else if (this.imageIdxSelection === 'end') {
+          this.imageEndIdx = idx;
+        }
+      }
+    },
   },
 };
 </script>
@@ -380,6 +452,5 @@ export default {
   position: sticky;
   top: 80px;
   z-index: 9;
-  background-color: white;
 }
 </style>
