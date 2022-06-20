fix: influence function fixed; fixed conflict with flashtorch; forced influence calculation to use dataLoaders with num_workers=0; need testing

back-end/modules/influence_module/calc_influence_function.py
@@ -8,7 +8,7 @@ import copy
 import logging
 
 from pathlib import Path
-from pytorch_influence_functions.influence_function import s_test, grad_z
+from .influence_function import s_test, grad_z
 from pytorch_influence_functions.utils import save_json, display_progress
 
 
@@ -355,6 +355,8 @@ def calc_influence_single(model, train_loader, test_loader, test_id_num, gpu,
             max_influence_dict = dict_slice(max_influence_dict, 0, 4)
 
         display_progress("Calc. influence function: ", i, train_dataset_size)
+
+
 
     harmful = np.argsort(influences)
     helpful = harmful[::-1]

back-end/modules/influence_module/influence_function.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python3
 
+from regex import W
 import torch
 from torch.autograd import grad
-from utils import display_progress
+from pytorch_influence_functions.utils import display_progress
 
 
 def s_test(z_test, t_test, model, z_loader, gpu=-1, damp=0.01, scale=25.0,
@@ -43,8 +44,8 @@ def s_test(z_test, t_test, model, z_loader, gpu=-1, damp=0.01, scale=25.0,
                 x, t = x.cuda(), t.cuda()
             y = model(x)
             loss = calc_loss(y, t)
-            params = [ p for p in model.parameters() if p.requires_grad ]
-            hv = hvp(loss, params, h_estimate)
+            # params = [ p for p in model.parameters() if p.requires_grad ]
+            hv = hvp(loss, list(model.parameters()), h_estimate)
             # Recursively caclulate h_estimate
             h_estimate = [
                 _v + (1 - damp) * _h_e - _hv / scale
@@ -94,8 +95,11 @@ def grad_z(z, t, model, gpu=-1):
     y = model(z)
     loss = calc_loss(y, t)
     # Compute sum of gradients from model parameters to loss
-    params = [ p for p in model.parameters() if p.requires_grad ]
-    return list(grad(loss, params, create_graph=True))
+
+    # params = [ p for p in model.parameters() if p.requires_grad ]
+    # print(params)
+
+    return list(grad(loss, list(model.parameters()), create_graph=True))
 
 
 def hvp(y, w, v):

back-end/utils/predict.py
@@ -3,7 +3,9 @@ from objects.RModelWrapper import RModelWrapper
 import torch
 import modules.influence_module as ptif
 import threading
+import time
 from objects.RDataManager import RDataManager
+from objects.RTask import RTask, TaskType
 import pickle
 from utils.path_utils import to_unix
 
@@ -37,7 +39,6 @@ def get_image_prediction(modelWrapper: RModelWrapper, imgpath: str, imgsize: int
     image = image.to(modelWrapper.device)
 
     model = modelWrapper.model
-
     out_score = model(image) # size: (1, num_classes). For imageNet, shape is (1, 10)
 
     if argmax:
@@ -60,10 +61,18 @@ def calculate_influence(modelWrapper:RModelWrapper, dataManager:RDataManager, st
                 influence for the entire dataset.
     """
 
+    task = RTask(TaskType.AutoAnnotate, end_idx - start_idx)
+    starttime = time.time()
     influence_buffer = dataManager.get_influence_buffer()
 
-    trainloader = dataManager.trainloader
-    testloader = dataManager.testloader
+    # Force num_workers to be 0 to prevent error
+    testloader = torch.utils.data.DataLoader(
+        dataManager.testset, batch_size=dataManager.batch_size, shuffle=False, num_workers=0)
+    trainloader = torch.utils.data.DataLoader(
+        dataManager.trainset, batch_size=dataManager.batch_size, shuffle=False, num_workers=0)
+
+    # trainloader = dataManager.trainloader
+    # testloader = dataManager.testloader
 
     if end_idx == -1:
         end_idx = len(testloader.dataset)
@@ -92,16 +101,9 @@ def calculate_influence(modelWrapper:RModelWrapper, dataManager:RDataManager, st
         lst = [p[0] for p in lst] 
         influence_buffer.set(testloader.dataset[idx][0], [to_unix(trainloader.dataset[int(train_idx)][0]) for train_idx in lst])
 
-    
-class CalcInfluenceThread(threading.Thread):
-    def __init__(self, modelWrapper:RModelWrapper, dataManager:RDataManager, start_idx, end_idx, r_averaging):
-        super(CalcInfluenceThread, self).__init__()
-        self.modelWrapper = modelWrapper
-        self.dataManager = dataManager
-        self.start_idx = start_idx
-        self.end_idx = end_idx
-        self.r_averaging = r_averaging
-        self.stop = True
-
-    def run(self):
-        calculate_influence(self.modelWrapper, self.dataManager, self.start_idx, self.end_idx, self.r_averaging)
+        task_update_res = task.update()
+        if not task_update_res:
+            endtime = time.time()
+            print("Time consumption:", endtime-starttime)
+            print("Influence calculation stopped")
+            return 