c7ba223b13e7bd26ed2a421299f387b06eb9ba3f
Chonghan <paulcccccch@gmail.com>
2022-10-02 14:20:32 -0400

    fixed frontend test case: now wait until auto annotation is finished

--- front-end/cypress/tests/components/auto_annotate_pad.spec.js
+++ front-end/cypress/tests/components/auto_annotate_pad.spec.js
@@ -16,8 +16,13 @@ describe('Auto Annotate Pad', () => {
   it('Test Annotated Images url', () => {
     cy.getBySel('auto-annotate-start-index').clear().type('1');
     cy.getBySel('auto-annotate-end-index').clear().type('3');
-    cy.clickBySel('auto-annotate-pad-start-auto-annotation');
-    cy.wait(20000);
+    cy.getBySel('header-toggle-tasks-panel').click();
+    cy.getBySel('auto-annotate-pad-start-auto-annotation').click();
+
+    // This will wait for max 120 seconds until the task is finished
+    cy.get('task-panel-stop-task'); // Wait until task pops up
+    cy.get('[data-test=task-center-p-no-task]', { timeout: 120 * 1000 });
+
     cy.contains('Inspect Data').click();
     cy.contains('Annotated Data').click();
     cy.url().should('include', '/image-list/annotated');
@@ -33,14 +38,14 @@ describe('Auto Annotate Pad', () => {
     cy.clickBySel('auto-annotate-pad-start-auto-annotation');
     cy.getBySel('task-panel-item-name').children().should('have.length', 1);
     cy.clickBySel('task-panel-stop-task');
-    cy.get('p').should('have.text', 'No task is running now.');
+    cy.getBySel('task-center-p-no-task').should('be.visible');
   });
 
   it('Test Input Zero', () => {
     cy.getBySel('header-toggle-tasks-panel').click();
     cy.getBySel('auto-annotate-end-index').clear().type('0');
     cy.clickBySel('auto-annotate-pad-start-auto-annotation');
-    cy.get('p').should('have.text', 'No task is running now.');
+    cy.getBySel('task-center-p-no-task').should('be.visible');
   });
 
   it('Test Input Zero Before Integer', () => {
@@ -64,21 +69,21 @@ describe('Auto Annotate Pad', () => {
     cy.getBySel('header-toggle-tasks-panel').click();
     cy.getBySel('auto-annotate-end-index').clear().type('9.9');
     cy.clickBySel('auto-annotate-pad-start-auto-annotation');
-    cy.get('p').should('have.text', 'No task is running now.');
+    cy.getBySel('task-center-p-no-task').should('be.visible');
   });
 
   it('Test Start index be negative number', () => {
     cy.getBySel('header-toggle-tasks-panel').click();
     cy.getBySel('auto-annotate-start-index').clear().type('-1');
     cy.getBySel('auto-annotate-pad-start-auto-annotation').click();
-    cy.get('p').should('have.text', 'No task is running now.');
+    cy.getBySel('task-center-p-no-task').should('be.visible');
   });
 
   it('Test End index be less than -1', () => {
     cy.getBySel('header-toggle-tasks-panel').click();
     cy.getBySel('auto-annotate-end-index').clear().type('-2');
     cy.getBySel('auto-annotate-pad-start-auto-annotation').click();
-    cy.get('p').should('have.text', 'No task is running now.');
+    cy.getBySel('task-center-p-no-task').should('be.visible');
   });
 
   it('Test End index be less than Start index', () => {
@@ -86,6 +91,6 @@ describe('Auto Annotate Pad', () => {
     cy.getBySel('auto-annotate-start-index').clear().type('99999');
     cy.getBySel('auto-annotate-end-index').clear().type('99998');
     cy.getBySel('auto-annotate-pad-start-auto-annotation').click();
-    cy.get('p').should('have.text', 'No task is running now.');
+    cy.getBySel('task-center-p-no-task').should('be.visible');
   });
 });
