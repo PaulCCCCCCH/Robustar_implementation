a3e3fe08f2565715d00f7f5c0fcddfaeb8375400
Chonghan <paulcccccch@gmail.com>
2022-10-02 16:53:19 -0400

    fixed prediction bug: added locks around visualization to avoid conflict

--- back-end/modules/visualize_module/visualize/visual.py
+++ back-end/modules/visualize_module/visualize/visual.py
@@ -1,3 +1,4 @@
+from objects.RModelWrapper import RModelWrapper
 from ..flashtorch_.utils import apply_transforms, load_image
 from ..flashtorch_.saliency import Backprop
 import torch, matplotlib
@@ -7,18 +8,26 @@ import matplotlib.pyplot as plt
 
 
 # https://github.com/MisaOgura/flashtorch#saliency-maps-flashtorchsaliency
-def visualize(mymodel, imgpath, imgsize, device):
-    backprop = Backprop(mymodel)
-    image = load_image(imgpath)
-    image = apply_transforms(image, imgsize)
+def visualize(modelWrapper: RModelWrapper, imgpath, imgsize, device):
+    mymodel = modelWrapper.model
+    modelWrapper.lock.acquire()
+    images = []
+    try: 
+        backprop = Backprop(mymodel)
+        image = load_image(imgpath)
+        image = apply_transforms(image, imgsize)
 
-    modeloutput = backprop.model(image.to(device))
-    modeloutput = torch.nn.functional.softmax(modeloutput, 1)
+        modeloutput = backprop.model(image.to(device))
+        modeloutput = torch.nn.functional.softmax(modeloutput, 1)
 
-    _, predict = torch.max(modeloutput, 1)
-    use_gpu = device != 'cpu'
-    images = backprop.visualize(image, predict, guided=True, return_image=True, use_gpu=use_gpu)
-    backprop.unregister_hooks()
+        _, predict = torch.max(modeloutput, 1)
+        use_gpu = device != 'cpu'
+        images = backprop.visualize(image, predict, guided=True, return_image=True, use_gpu=use_gpu)
+        backprop.unregister_hooks()
+    except Exception as e:
+        print(str(e))
+    finally:
+        modelWrapper.lock.release()
 
     return images
 
