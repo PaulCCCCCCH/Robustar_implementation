375be54d54db42d791164e639da9533c94bc3479
Chonghan <paulcccccch@gmail.com>
2022-12-03 23:40:44 -0500

    changed entrypoint

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,4 @@ service nginx restart
 
 # Start backend server
 cd back-end
-nohup nginx -g "daemon off;" & gunicorn -b 0.0.0.0:8000 --workers=1 "server:create_app()" "$1"
+nohup nginx -g "daemon off;" & gunicorn -b 0.0.0.0:8000 --workers=1 "server:app" "$1"
