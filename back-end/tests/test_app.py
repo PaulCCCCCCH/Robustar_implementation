407502b679b66b1c31096fdfeed8d0c9c15f4be1
Yuhao Zhang <yhao.zhang98@gmail.com>
2022-12-04 15:48:12 +0000

    TEST: adds validation set for tests

--- back-end/tests/test_app.py
+++ back-end/tests/test_app.py
@@ -31,7 +31,7 @@ def app(request):
 
     _clean_up(basedir)
 
-    # time.sleep(0.5)
+    time.sleep(0.1)
 
 
 @pytest.fixture()
@@ -56,13 +56,13 @@ def _set_up(basedir):
             image_dir = to_unix(osp.join(train_dir, name))
             image_dir_original = to_unix(osp.join(train_dir_original, name))
             if not osp.isdir(image_dir_original):
-            # if len(image_dir.split('/')[-1].split('.')) > 1:
                 continue
             os.mkdir(image_dir)
             for i in range(10):
                 image = to_unix(osp.join(image_dir, "{}.JPEG".format(i)))
                 image_original = to_unix(osp.join(image_dir_original, "{}.JPEG".format(i)))
                 shutil.copy2(image_original, image)
+        print("setup >> test train set created")
         test_dir = to_unix(osp.join(dataset_dir, 'test'))
         test_dir_original = to_unix(osp.join(dataset_dir_original, 'test'))
         os.mkdir(test_dir)
@@ -70,13 +70,26 @@ def _set_up(basedir):
             image_dir = to_unix(osp.join(test_dir, name))
             image_dir_original = to_unix(osp.join(test_dir_original, name))
             if not osp.isdir(image_dir_original):
-            # if len(image_dir.split('/')[-1].split('.')) > 1:
                 continue
             os.mkdir(image_dir)
             for i in range(10):
                 image = to_unix(osp.join(image_dir, "{}.JPEG".format(i)))
                 image_original = to_unix(osp.join(image_dir_original, "{}.JPEG".format(i)))
                 shutil.copy2(image_original, image)
+        print("setup >> test test set created")
+        validation_dir = to_unix(osp.join(dataset_dir, 'validation'))
+        os.mkdir(validation_dir)
+        for name in os.listdir(test_dir_original):
+            image_dir = to_unix(osp.join(validation_dir, name))
+            image_dir_original = to_unix(osp.join(test_dir_original, name))
+            if not osp.isdir(image_dir_original):
+                continue
+            os.mkdir(image_dir)
+            for i in range(10, 20):
+                image = to_unix(osp.join(image_dir, "{}.JPEG".format(i)))
+                image_original = to_unix(osp.join(image_dir_original, "{}.JPEG".format(i)))
+                shutil.copy2(image_original, image)
+        print("setup >> test validation set created")
         print("setup > copy images into " + dataset_dir)
     else:
         print("setup > no database dir, skip copy")
@@ -173,63 +186,4 @@ def _clean_up(basedir):
     # db_path = to_unix(osp.join(base_dir, 'data.db'))
     # db_path_original = to_unix(osp.join(base_dir, 'data_o.db'))
 
-# Reserve 10 photos for each category in trainset and 10 photos for each category in testset to save time
-# class TestTrain:
-#     # Test if the model is loaded correctly at weight level
-#     def test_load_model_correctness(self, client, server):
-#         assert server.getModelsWeights() == {}
-#
-#         data = {
-#             'info': 'placeholder',
-#             'configs': {
-#                 'model_name': 'my-test-model',
-#                 'weight': '',
-#                 'train_path': '/Robustar2/dataset/train-2',
-#                 'test_path': '/Robustar2/dataset/test-2',
-#                 'class_path': './model/cifar-class.txt',
-#                 'port': '8000',
-#                 'save_dir': '/Robustar2/checkpoints',
-#                 'use_paired_train': False,
-#                 'mixture': 'random_pure',
-#                 'paired_data_path': '/Robustar2/dataset/paired',
-#                 'auto_save_model': True,
-#                 'batch_size': '128',
-#                 'shuffle': True,
-#                 'learn_rate': 0.1,
-#                 'pgd': 'no PGD',
-#                 'paired_train_reg_coeff': 0.001,
-#                 'image_size': 32,
-#                 'epoch': 2,
-#                 'thread': 8,
-#                 'pretrain': False,
-#                 'user_edit_buffering': False,
-#                 'save_every': 1
-#             }
-#         }
-#
-#         rv = client.post("/train", json=data).get_json()
-#         assert rv['code'] == 0
-#         assert rv['data'] == 'Training started!'
-#         assert rv['msg'] == 'Success'
-#
-#         # Wait for the training
-#         time.sleep(25)
-#
-#         # Compare model weights saved in local path and in memory
-#         for name, weight in server.getModelsWeights().items():
-#             # Get the model weights saved in local path
-#             model_arch = server.getServerConfigs()['model_arch']
-#             net_path = os.path.join(server.ckptDir, name).replace('\\', '/')
-#             device = server.getServerConfigs()['device']
-#             pre_trained = server.getServerConfigs()['pre_trained']
-#             num_classes = server.getServerConfigs()['num_classes']
-#             modelWrapper = RModelWrapper(model_arch, net_path, device, pre_trained, num_classes)
-#             modelLoaded = modelWrapper.model
-#             weightLoaded = modelLoaded.state_dict()
-#
-#             # Get the model weights saved in memory
-#             weightInMem = server.getModelsWeights()[name]
-#
-#             # Compare each item in them
-#             for key_item_1, key_item_2 in zip(weightLoaded.items(), weightInMem.items()):
-#                 assert torch.equal(key_item_1[1], key_item_2[1])
+
--- back-end/tests/test_train.py
+++ back-end/tests/test_train.py
@@ -1,4 +1,149 @@
+import os
+import time
+
+import torch
+
 from test_app import app, client
 
 
 # class TestTrain:
+#     # Test if the model is loaded correctly at weight level
+#     def test_load_model_correctness(self, client, server):
+#         assert server.getModelsWeights() == {}
+#
+#         data = {
+#             'info': 'placeholder',
+#             'configs': {
+#                 'model_name': 'my-test-model',
+#                 'weight': '',
+#                 'train_path': '/Robustar2/dataset/train-10',
+#                 'test_path': '/Robustar2/dataset/test-10',
+#                 'class_path': './model/cifar-class.txt',
+#                 'port': '8000',
+#                 'save_dir': '/Robustar2/checkpoints',
+#                 'use_paired_train': False,
+#                 'mixture': 'random_pure',
+#                 'paired_data_path': '/Robustar2/dataset/paired',
+#                 'auto_save_model': True,
+#                 'batch_size': '27',
+#                 'shuffle': True,
+#                 'learn_rate': 0.003,
+#                 'pgd': 'no PGD',
+#                 'paired_train_reg_coeff': 0.001,
+#                 'image_size': 32,
+#                 'epoch': 5,
+#                 'thread': 4,
+#                 'pretrain': False,
+#                 'user_edit_buffering': False,
+#                 'save_every': 1
+#             }
+#         }
+#
+#         rv = client.post("/train", json=data).get_json()
+#         assert rv['code'] == 0
+#         assert rv['data'] == 'Training started!'
+#         assert rv['msg'] == 'Success'
+#
+#         # Wait for the training
+#         time.sleep(30)
+#
+#         # Compare model weights saved in local path and in memory
+#         for name, weight in server.getModelsWeights().items():
+#             # Get the model weights saved in local path
+#             model_arch = server.getServerConfigs()['model_arch']
+#             net_path = os.path.join(server.ckptDir, name).replace('\\', '/')
+#             device = server.getServerConfigs()['device']
+#             pre_trained = server.getServerConfigs()['pre_trained']
+#             num_classes = server.getServerConfigs()['num_classes']
+#             modelWrapper = RModelWrapper(model_arch, net_path, device, pre_trained, num_classes)
+#             modelLoaded = modelWrapper.model
+#             weightLoaded = modelLoaded.state_dict()
+#
+#             # Get the model weights saved in memory
+#             weightInMem = server.getModelsWeights()[name]
+#
+#             # Compare each item in them
+#             for key_item_1, key_item_2 in zip(weightLoaded.items(), weightInMem.items()):
+#                 assert torch.equal(key_item_1[1], key_item_2[1])
+#
+#     # Test whether training works correctly
+#     def test_train_model(self, client, server):
+#         # Test non-paired training
+#         data = {
+#             'info': 'placeholder',
+#             'configs': {
+#                 'model_name': 'test-regular-training',
+#                 'weight': '',
+#                 'train_path': '/Robustar2/dataset/train-10',
+#                 'test_path': '/Robustar2/dataset/test-10',
+#                 'class_path': './model/cifar-class.txt',
+#                 'port': '8000',
+#                 'save_dir': '/Robustar2/checkpoints',
+#                 'use_paired_train': False,
+#                 'mixture': 'random_pure',
+#                 'paired_data_path': '/Robustar2/dataset/paired',
+#                 'auto_save_model': True,
+#                 'batch_size': '27',
+#                 'shuffle': True,
+#                 'learn_rate': 0.003,
+#                 'pgd': 'no PGD',
+#                 'paired_train_reg_coeff': 0.001,
+#                 'image_size': 32,
+#                 'epoch': 5,
+#                 'thread': 4,
+#                 'pretrain': False,
+#                 'user_edit_buffering': False,
+#                 'save_every': 1
+#             }
+#         }
+#
+#         rv = client.post("/train", json=data).get_json()
+#         assert rv['code'] == 0
+#         assert rv['data'] == 'Training started!'
+#         assert rv['msg'] == 'Success'
+#
+#         # Wait for the training
+#         time.sleep(30)
+#
+#         # Check the improvement of accuracy
+#         assert server.getTrainedAcc() > server.getInitAcc()
+#
+#         # Test paired training
+#         data = {
+#             'info': 'placeholder',
+#             'configs': {
+#                 'model_name': 'test-regular-training',
+#                 'weight': '',
+#                 'train_path': '/Robustar2/dataset/train-10',
+#                 'test_path': '/Robustar2/dataset/test-10',
+#                 'class_path': './model/cifar-class.txt',
+#                 'port': '8000',
+#                 'save_dir': '/Robustar2/checkpoints',
+#                 'use_paired_train': True,
+#                 'mixture': 'random_pure',
+#                 'paired_data_path': '/Robustar2/dataset/paired-10',
+#                 'auto_save_model': True,
+#                 'batch_size': '27',
+#                 'shuffle': True,
+#                 'learn_rate': 0.003,
+#                 'pgd': 'no PGD',
+#                 'paired_train_reg_coeff': 0.001,
+#                 'image_size': 32,
+#                 'epoch': 5,
+#                 'thread': 4,
+#                 'pretrain': False,
+#                 'user_edit_buffering': False,
+#                 'save_every': 1
+#             }
+#         }
+#
+#         rv = client.post("/train", json=data).get_json()
+#         assert rv['code'] == 0
+#         assert rv['data'] == 'Training started!'
+#         assert rv['msg'] == 'Success'
+#
+#         # Wait for the training
+#         time.sleep(30)
+#
+#         # Check the improvement of accuracy
+#         assert server.getTrainedAcc() > server.getInitAcc()
