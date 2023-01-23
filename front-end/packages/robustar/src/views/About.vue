34dbece262d6f740960b164b7ebeca1090b24d23
Desmond <scysl4@nottingham.edu.cn>
2023-01-22 19:25:54 -0800

    feat: improved and unified some styles

--- front-end/packages/robustar/src/views/About.vue
+++ front-end/packages/robustar/src/views/About.vue
@@ -1,74 +1,80 @@
 <template>
-  <div class="container">
-    <div class="d-flex flex-wrap justify-space-around">
-      <v-card elevation="2" outlined class="px-15">
-        <v-card-title> <h1>Robustar Version: v0.2.0</h1> </v-card-title>
-        <v-card-subtitle> Released: Jan 15, 2023 </v-card-subtitle>
+  <div class="d-flex flex-column align-center">
+    <v-card elevation="2" outlined class="my-8 pa-8">
+      <v-card-title> <h2>Robustar Version: v0.2.0</h2> </v-card-title>
+      <v-card-subtitle> Released: Jan 15, 2023 </v-card-subtitle>
 
-        <!-- Resources -->
-        <v-divider class="mt-4"></v-divider>
-        <v-card-text>
-          <h2>Resources</h2>
-          <v-btn text href="https://haohanwang.github.io/Robustar/" depressed color="primary">
-            Documentation and Tutorial
-          </v-btn>
-          <br />
+      <!-- Resources -->
+      <v-divider class="mt-4"></v-divider>
 
-          <v-btn
-            text
-            href="https://github.com/PaulCCCCCCH/Robustar_implementation"
-            depressed
-            color="primary"
-          >
-            Source Code
-          </v-btn>
-        </v-card-text>
+      <v-card-text>
+        <h2 class="mb-2">Resources</h2>
+        <v-btn
+          text
+          href="https://haohanwang.github.io/Robustar/"
+          target="_blank"
+          depressed
+          color="primary"
+        >
+          Documentation and Tutorial
+        </v-btn>
+        <br />
 
-        <!-- Bug report -->
-        <v-divider class="mt-4"></v-divider>
+        <v-btn
+          text
+          href="https://github.com/PaulCCCCCCH/Robustar_implementation"
+          target="_blank"
+          depressed
+          color="primary"
+        >
+          Source Code
+        </v-btn>
+      </v-card-text>
 
-        <v-card-text>
-          <h2>Saw a bug?</h2>
+      <!-- Bug report -->
+      <v-divider class="mt-4"></v-divider>
 
-          <v-btn text href="mailto:chonghac@andrew.cmu.edu" depressed color="primary">
-            Send us an email
-          </v-btn>
+      <v-card-text>
+        <h2 class="mb-2">Saw a bug?</h2>
 
-          or
-          <v-btn
-            text
-            href="https://github.com/PaulCCCCCCH/Robustar_implementation/issues/new"
-            depressed
-            color="primary"
-          >
-            Raise an issue here
-          </v-btn>
-        </v-card-text>
+        <v-btn text href="mailto:chonghac@andrew.cmu.edu" target="_blank" depressed color="primary">
+          Send us an email
+        </v-btn>
 
-        <!-- More about us -->
-        <v-divider class="mt-4"></v-divider>
-        <v-card-text>
-          <h2>More about us</h2>
-          <h5>
-            We are a group of enthusiastic open-source contributors based off the USA, UK and China.
-            <br />
-            Follow us on:
-          </h5>
+        or
+        <v-btn
+          text
+          href="https://github.com/PaulCCCCCCH/Robustar_implementation/issues/new"
+          target="_blank"
+          depressed
+          color="primary"
+        >
+          Raise an issue here
+        </v-btn>
+      </v-card-text>
 
-          <v-btn class="mb-4" icon>
-            <v-icon>mdi-github</v-icon>
-          </v-btn>
+      <!-- More about us -->
+      <v-divider class="mt-4"></v-divider>
 
-          <v-btn class="mb-4" icon>
-            <v-icon>mdi-twitter</v-icon>
-          </v-btn>
+      <v-card-text>
+        <h2 class="mb-4">More about us</h2>
+        We are a group of enthusiastic open-source contributors based off the USA, UK and China.
+        <br />
+        Follow us on:
 
-          <v-btn class="mb-4" icon>
-            <v-icon>mdi-instagram</v-icon>
-          </v-btn>
-        </v-card-text>
-      </v-card>
-    </div>
+        <v-btn class="mb-4" icon>
+          <v-icon>mdi-github</v-icon>
+        </v-btn>
+
+        <v-btn class="mb-4" icon>
+          <v-icon>mdi-twitter</v-icon>
+        </v-btn>
+
+        <v-btn class="mb-4" icon>
+          <v-icon>mdi-instagram</v-icon>
+        </v-btn>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
