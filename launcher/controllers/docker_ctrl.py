6131dfffc4f3e74d329540ec26fe12c87f548ac1
Leon-Leyang <scylh6@nottingham.edu.cn>
2023-02-11 14:58:13 +0800

    Change the global record data folder on mac os to `~/Library/Application Support/RobustarLauncher` #139

--- launcher/controllers/docker_ctrl.py
+++ launcher/controllers/docker_ctrl.py
@@ -29,7 +29,7 @@ class DockerController(QObject):
             self.api_client = docker.APIClient(base_url="tcp://localhost:2375")
             self.root = os.path.join(home_dir, "AppData", "Local", "RobustarLauncher")
         elif platform == "darwin":
-            self.root = os.path.join(home_dir, "Library", "Containers", "RobustarLauncher")
+            self.root = os.path.join(home_dir, "Library", "Application Support", "RobustarLauncher")
             pass
 
         # Synchronize the record data
