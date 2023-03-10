4b7feae42a060c51234dc46f6555b8fa8fce74b7
Chonghan Chen <33018020+PaulCCCCCCH@users.noreply.github.com>
2023-03-10 16:54:52 -0500

    Some improvements on code quality (#149)
    * Removed some useless code and files
    
    * Made prediction use data_manager's transformation for consistency; removed some useless code
    
    * Corrected image shape for prediction
    
    * Added image shape precheck and auto-adjustment according to model type
    
    * Bug fix: self.batch_size = image_size

--- back-end/objects/RModelWrapper.py
+++ back-end/objects/RModelWrapper.py
@@ -5,6 +5,17 @@ from threading import Lock
 
 IMAGENET_OUTPUT_SIZE = 1000
 
+MODEL_INPUT_SHAPE = {
+    "resnet-18": 224,
+    "resnet-34": 224,
+    "resnet-50": 224,
+    "resnet-101": 224,
+    "resnet-152": 224,
+    "mobilenet-v2": 224,
+    "resnet-18-32x32": 32,
+    "alexnet": 227,
+}
+
 
 class RModelWrapper:
     def __init__(self, network_type, net_path, device, pretrained, num_classes):
@@ -76,6 +87,8 @@ class RModelWrapper:
             self.model.load_state_dict(torch.load(path, map_location=self.device))
         else:
             print("weight file not found")
+<<<<<<< HEAD
+=======
 
     def acquire_model(self):
         """
@@ -102,3 +115,4 @@ class RModelWrapper:
         self._lock.acquire()
         self._model_available = True
         self._lock.release()
+>>>>>>> dev
