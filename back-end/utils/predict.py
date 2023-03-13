37d9743e3e01ea22d90c9cd4a95005e3e7793c74
jyanln <21376182+jyanln@users.noreply.github.com>
2023-03-13 02:21:57 -0400

    Update influence function module to use alstonlo/torch-influence (#145)
    * Update influence function module to use alstonlo/torch-influence
    - Fix influence NaN calculation
    - Optimize calculation speed (still slow due to dataset size)
    - Update outdated parts of influence API
    
    * Fix influence module import issue
    
    * Now able to start/stop influence calc with task center (#153)
    
    ---------
    
    Co-authored-by: Chonghan Chen <33018020+PaulCCCCCCH@users.noreply.github.com>

--- back-end/utils/predict.py
+++ back-end/utils/predict.py
@@ -77,8 +77,6 @@ def calculate_influence(
     """
 
     INFLUENCES_SAVE_PATH = data_manager.influence_file_path
-    influences = {}
-
     trainloader = data_manager.trainloader
     testloader = data_manager.testloader
 
@@ -110,22 +108,36 @@ def calculate_influence(
 
     # TODO: change argument from testloader to misclassified test loader
     # as we are only interested in the influence for misclassified samples
-    max_influence_dicts = ptif.calc_img_wise(
-        config, model_wrapper.model, trainloader, testloader
-    )
+
+
+    # TODO: Now max_influence_dicts will be empty if user stops the influence calculation
+    # (i.e., all previous results are thrown away because we only return the full result when
+    # influence is calculated for all specified images). Maybe we should instead return influence
+    # for each image one by one to build the dictionary slowly, so that when calculation stops,
+    # we can save what has been calculated.
+
+    influences = {}
+    max_influence_dicts = {}
+    try:
+        max_influence_dicts = ptif.calc_img_wise(
+            config, model_wrapper.model, trainloader, testloader
+        )
+    except StopIteration:
+        print("Influence calculation stopped!")
 
     for key in max_influence_dicts.keys():
         train_img_paths = []
 
-        testId = "test/" + key
-        test_img_path = imageURLToPath(testId)
+        test_id = int(key)
+        test_img_path = data_manager.testset.get_image_list(test_id, test_id + 1)[0]
 
         max_influence_dict = max_influence_dicts[key]
         trainIds = list(max_influence_dict.keys())
 
         for j in range(4):
             trainUrl = "train/" + str(trainIds[j])
-            train_img_path = imageURLToPath(trainUrl)
+            train_id = int(trainIds[j])
+            train_img_path = data_manager.trainset.get_image_list(train_id, train_id + 1)[0]
             # TODO: Stores both image path and image url.
             # Adding / removing samples to training set will cause inconsistency
             # Need to check consistency in data manager when loading.
@@ -133,8 +145,9 @@ def calculate_influence(
 
         influences[test_img_path] = train_img_paths
 
-    with open(INFLUENCES_SAVE_PATH, "wb") as influence_file:
-        pickle.dump(influences, influence_file)
+    if influences:
+        with open(INFLUENCES_SAVE_PATH, "wb") as influence_file:
+            pickle.dump(influences, influence_file)
 
 
 class CalcInfluenceThread(threading.Thread):
--- scripts/install_pytorch.sh
+++ scripts/install_pytorch.sh
@@ -11,7 +11,7 @@ function getTorchCPU {
 }
 
 function getTorchCUDA92 {
-    pip install \
+    python3.9 -m pip install \
     torch==1.7.0+cu92 \
     torchvision==0.8.0+cu92 \
     torchattacks==3.1.0 \
@@ -21,7 +21,7 @@ function getTorchCUDA92 {
 }
 
 function getTorchCUDA102 {
-    pip install \
+    python3.9 -m pip install \
     torch==1.9.1+cu102 \
     torchvision==0.10.1+cu102  \
     torchattacks==3.1.0 \
@@ -31,7 +31,7 @@ function getTorchCUDA102 {
 }
 
 function getTorchCUDA111 {
-    pip install \
+    python3.9 -m pip install \
     torch==1.9.1+cu111 \
     torchvision==0.10.1+cu111  \
     torchattacks==3.1.0 \
@@ -42,8 +42,8 @@ function getTorchCUDA111 {
 
 function getTorchCUDA113 {
     pip install \
-    torch==1.9.1+cu113 \
-    torchvision==0.10.1+cu113  \
+    torch==1.12.1+cu113 \
+    torchvision==0.13.1+cu113  \
     torchattacks==3.1.0 \
     pytorch_influence_functions==0.1.1 \
     flashtorch==0.1.3 \
@@ -62,7 +62,7 @@ function installPyTorch {
     elif [ "$1" == '11.3' ]; then
         getTorchCUDA113
     else
-        echo "CUDA version not supported."
+        echo "CUDA version $1 not supported."
         exit 1
     fi
 }
