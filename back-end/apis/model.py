1b4edfe0e367ef0b559e276a6e6c878a9121bdb5
Leon-Leyang <scylh6@nottingham.edu.cn>
2023-07-30 22:25:57 +0800

    Add 'model_utils.py' for utilities & outline 'UploadModel' API

--- back-end/apis/model.py
+++ back-end/apis/model.py
@@ -1,6 +1,11 @@
 import string
+import os
+import torch
 from flask import request
 from flask import Blueprint
+from utils.model_utils import init_model_from_def, val_model, save_model
+from objects.RResponse import RResponse
+from objects.RServer import RServer
 
 model_api = Blueprint("model_api", __name__)
 
@@ -87,13 +92,78 @@ def UploadModel():
     """
     print("Requested to upload a model")
 
-    # Get the model definition code
+    # Get the model definition code and save it to a temporary file
     model_def = request.form.get('model_def')
+    # TODO: discuss with team the path to save the temp file
+    def_file_path = os.path.join(RServer.get_server().base_dir, 'temp_def.py')
+    try:
+        with open(def_file_path, 'w') as temp_file:
+            temp_file.write(model_def)
+    except Exception as e:
+        # Delete the temporary definition file and return
+        os.remove(def_file_path)
+        return RResponse.abort(500, f"Failed to save the model definition. {e}")
 
-    # Get the weight file
+    # Initialize the model
+    try:
+        model = init_model_from_def(def_file_path)
+    except Exception as e:
+        # Delete the temporary definition file and return
+        os.remove(def_file_path)
+        return RResponse.abort(400, f"Failed to initialize the model. {e}")
+
+    # Get the weight file and save it to a temporary location if it exists
     weight_file = request.files.get('weight_file')
+    if weight_file is not None:
+        try:
+            weight_file_path = os.path.join(RServer.get_server().base_dir, 'temp_weights.pth')
+            weight_file.save(weight_file_path)
+        except Exception as e:
+            # Delete the weight file
+            os.remove(weight_file_path)
+            return RResponse.abort(500, f"Failed to save the weight file. {e}")
 
-    pass
+    # Load the weights from the file
+    try:
+        model.load_state_dict(torch.load(weight_file_path))
+    except Exception as e:
+        # Delete the temp files
+        os.remove(def_file_path)
+        os.remove(weight_file_path)
+        return RResponse.abort(400, f"Failed to load the weight. {e}")
+
+    # Validate the model
+    try:
+        val_model(model)
+    except Exception as e:
+        # Delete the temp files
+        os.remove(def_file_path)
+        os.remove(weight_file_path)
+        return RResponse.abort(400, f"The model is invalid. {e}")
+
+    # TODO: generate a real model id
+    model_id = "abc123"
+
+    # Save the model to the database
+    try:
+        save_model(model)
+    except Exception as e:
+        # Delete the temp files
+        os.remove(def_file_path)
+        os.remove(weight_file_path)
+        return RResponse.abort(500, f"Failed to save the model. {e}")
+
+    # Set the current model to the newly uploaded model
+    try:
+        SetCurrModel(model_id)
+    except Exception as e:
+        # Delete the temp files
+        os.remove(def_file_path)
+        os.remove(weight_file_path)
+        return RResponse.abort(500, f"Failed to set the current model. {e}")
+
+    # TODO: return real data as specified in the docstring
+    return RResponse.ok('Success')
 
 
 @model_api.route("/model/list", methods=["GET"])
---
+++ back-end/utils/model_utils.py
@@ -0,0 +1,20 @@
+def init_model_from_def(def_file_path):
+    """ Initialize the model from the definition file
+    """
+    try:
+        pass
+    except Exception as e:
+        print("Failed to initialize the model.")
+        print(e)
+        raise e
+
+
+def val_model(model):
+    """ Validate the model by running the model against a small portion of the validation dataset
+    """
+    pass
+
+
+# TODO: align the arguments with the actual database design
+def save_model(model):
+    pass
