c38057374ed60cc871c219ccf447a363a352632d
Leon-Leyang <scylh6@nottingham.edu.cn>
2023-07-30 18:11:07 +0800

    Add docstrings for the UploadModel API

--- back-end/apis/model.py
+++ back-end/apis/model.py
@@ -37,16 +37,62 @@ def DeleteModel():
 
 @model_api.route("/model", methods=["POST"])
 def UploadModel():
-    """ Should also accept (optionally) a model weight file as argument """
-    """ After training a model, should do the same"""
-    """ return data
-        {
-            id: string,
-            name: string,
-            details: string,
-        }
+    """
+    Should also accept (optionally) a model weight file as argument
+    After training a model, should do the same
+    ---
+    tags:
+      - model
+    consumes:
+      - "multipart/form-data"
+    produces:
+      - "application/json"
+    parameters:
+      - in: "formData"        # Use "formData" to indicate multipart/form-data parameters
+        name: "model_def"     # Name of the parameter for the model definition code
+        description: "The definition of the model in Python code"
+        required: true
+        type: "string"        # The type of data for the model definition code
+      - in: "formData"        # Use "formData" to indicate multipart/form-data parameters
+        name: "weight_file"   # Name of the parameter for the model weight file
+        description: "The weight file for the trained model (optional)"
+        required: false       # The weight file is optional, so set 'required' to false
+        type: "file"          # The type of data for the weight file (file upload)
+
+    responses:
+      200:
+        description: Model uploaded successfully
+        schema:
+          type: "object"
+          properties:
+            code:
+              type: "integer"
+              example: 0
+            data:
+              type: "object"
+              properties:
+                id:
+                  type: "string"
+                  example: "abc123"
+                name:
+                  type: "string"
+                  example: "NewModel"
+                details:
+                  type: "string"
+                  example: "Uploaded successfully"
+            msg:
+              type: "string"
+              example: "Success"
 
     """
+    print("Requested to upload a model")
+
+    # Get the model definition code
+    model_def = request.form.get('model_def')
+
+    # Get the weight file
+    weight_file = request.files.get('weight_file')
+
     pass
 
 
