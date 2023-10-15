7990f58ca3287f8b9451f2ff9417a22238c611a9
Leon-Leyang <scylh6@nottingham.edu.cn>
2023-10-15 16:28:50 -0400

    Modify the test data preparation logic

--- back-end/conftest.py
+++ back-end/conftest.py
@@ -1,5 +1,5 @@
 def pytest_addoption(parser):
-    parser.addoption("--basedir", action="store", default="/Robustar2", help="Base directory where robustar folder is placed")
+    parser.addoption("--basedir", action="store", default="/", help="Base directory where the robustar test folder is unzipped")
 
 
 
