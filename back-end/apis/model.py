d6addb414dfdac0dc681353c87a11a09e256d5fd
Chonghan Chen <33018020+PaulCCCCCCH@users.noreply.github.com>
2023-12-17 20:02:28 -0800

    Update back-end/apis/model.py
    Co-authored-by: Leon-Leyang <82098977+Leon-Leyang@users.noreply.github.com>

--- back-end/apis/model.py
+++ back-end/apis/model.py
@@ -37,6 +37,7 @@ def delete_model(model_name: str):
     try:
         model = RServer.get_model_wrapper().delete_model_by_name(model_name)
     except Exception as e:
+        traceback.print_exc()
         RResponse.abort(500, f"Failed to delete model {model_name}. Error: {str(e)}")
 
     if not model:
